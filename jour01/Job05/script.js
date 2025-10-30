e562d79bf877e445316e210a9a207825481c40cd
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-30 13:55:06 +0100

    Job 05 réalisé avec succès

---
+++ jour01/Job05/script.js
@@ -0,0 +1,8 @@
+function afficherjourssemaines(){
+    const jourssemaines = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+    for (let i = 0; i < jourssemaines.length; i++){
+        console.log(jourssemaines[i]);
+    }
+}
+
+afficherjourssemaines();
