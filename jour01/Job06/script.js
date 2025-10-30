52ac219758712551c600210cc7b91c4efc872270
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-30 16:10:55 +0100

    Job06 réalisé avec succès

---
+++ jour01/Job06/script.js
@@ -0,0 +1,16 @@
+function fizzbuzz(){
+    for (let i = 0; i < 151; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        }else if(i % 3 === 0){
+            console.log("Fizz");
+        }else if (i % 5 === 0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        }
+        
+    }
+}
+
+fizzbuzz();
