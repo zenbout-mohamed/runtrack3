cc6a3c0d23cfcbae5158e3c76da1092aefb8fbb2
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-31 14:30:18 +0100

    génération de la balise textarea du fichier index.php

---
+++ jour02/Job04/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript-Jour02-Job04</title>
+    <script src="script.js" defer></script> 
+</head>
+<body>
+    <textarea id="keylogger" rows ="5" cols ="40" placeholder ="Tapez quelque chose..."></textarea>
+</body>
+</html>
