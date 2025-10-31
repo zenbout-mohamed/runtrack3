744a81f58c18e1a01bda270546f234c21607dc04
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-31 11:45:44 +0100

    job08 réalisé

---
+++ jour01/Job08/script.js
@@ -0,0 +1,22 @@
+function estPremier(n){
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if(n % i === 0) return false;
+        
+    }
+
+    return true ;
+    
+}
+
+
+function sommenombrespremiers(a , b){
+    if (estPremier(a) && estPremier(b)) {
+        return a + b ;
+    }else {
+        return false;
+    }
+}
+
+console.log(sommenombrespremiers(3 , 5));
+console.log(sommenombrespremiers(4 , 5));
