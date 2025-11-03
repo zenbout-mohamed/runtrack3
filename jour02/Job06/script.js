ef525b98b0a657128677e2d71fdee8bd0e2b79c8
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-03 14:47:01 +0100

    Job06 réalisé

--- jour02/Job06/script.js
+++ jour02/Job06/script.js
@@ -1,10 +1,17 @@
 const konamiCode = [
-    "ArrowUp" , "ArrowUp",
-    "ArrowDown" , "ArrowDown",
-    "ArrowLeft" , "ArrowRight",
-    "ArrowLeft" , "ArrowLeft",
-    "Left" , "Right"
+    "ArrowUp", "ArrowUp",
+    "ArrowDown", "ArrowDown",
+    "ArrowLeft", "ArrowRight",
+    "ArrowLeft", "ArrowRight",
+    "b", "a"
 ];
 
+let userInput = [];
 
+document.addEventListener("keydown", (e) => {
+    userInput.push(e.key);
 
+    if (userInput.slice(-konamiCode.length).join("") === konamiCode.join("")) {
+        document.body.classList.add("konami");
+    }
+});
