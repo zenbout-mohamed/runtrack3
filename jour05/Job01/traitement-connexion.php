ac57aa4b100aadc265c3a9a33913c652c9efef63
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-06 14:53:10 +0100

    page traitement-connexion.php corrigé

--- jour05/Job01/traitement-connexion.php
+++ jour05/Job01/traitement-connexion.php
@@ -3,35 +3,29 @@ require_once "config.php";
 session_start();
 
 header('Content-Type: application/json');
-$erreur = [];
+$errors = [];
 
 $email = trim($_POST['email'] ?? '');
 $password = $_POST['password'] ?? '';
 
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)) $erreur['email'] = "Email invalide.";
-if(empty($password)) $erreur['password'] = " Mot de passe requis ";
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['email'] = "Email invalide.";
+if (empty($password)) $errors['password'] = "Mot de passe requis.";
 
-if(empty($erreur)){
+if (empty($errors)) {
     $stmt = $pdo->prepare("SELECT * FROM utilisateurs_1 WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-
-    if($user && password_verify($password, $user['password'])){
+    if ($user && password_verify($password, $user['password'])) {
         $_SESSION['prenom'] = $user['prenom'];
         $_SESSION['id'] = $user['id'];
         echo json_encode(['success' => true]);
         exit;
     } else {
-        $erreur['email'] = "Identitfiant incorrect";
+        $errors['email'] = "Identifiant ou mot de passe incorrect.";
     }
 }
 
-echo json_encode(['success' => false, 'erreur' => $erreur]);
-
-
-
-
-
-
+echo json_encode(['success' => false, 'errors' => $errors]);
+exit;
 ?>
