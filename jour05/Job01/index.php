953b7c558a7a56b9fb34f3a14cfc20b75c257dea
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-06 10:46:20 +0100

    réalisation des pages traitement d'(inscription et connexion.php)

---
+++ jour05/Job01/index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript-Jour02-Job02</title>
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src ="script.js" defer></script> 
+</head>
+<body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
+    <section class="bg-white shadow-md rounded-lg p-8 text-center w-full max-w-md">
+        <?php if (isset($_SESSION['prenom'])): ?>
+            <h1 class="text-2xl text-teal-600 font-bold mb-4">Bonjour <?= htmlspecialchars($_SESSION['prenom']) ?> 👋</h1>
+            <a href="logout.php" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-500 transition">Déconnexion</a>
+        <?php else: ?>
+            <h1 class="text-2xl text-gray-700 font-bold mb-4">Bienvenue sur le site</h1>
+            <div class="flex justify-center gap-4">
+                <a href="inscription.php" class="bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-400 transition">Inscription</a>
+                <a href="connexion.php" class="bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-400 transition">Connexion</a>
+            </div>
+        <?php endif; ?>
+    </section>
+
+</body>
+</html>
---
+++ jour05/Job01/inscription.php
@@ -0,0 +1,54 @@
+<?php
+require_once "config.php";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript - Job02</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src ="script.js"></script>
+</head>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+<section class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+    <h2 class="text-2xl font-bold text-teal-600 mb-6 text-center">Créer un compte</h2>
+
+    <form id="formInscription" method="post" class="flex flex-col gap-4">
+        <div>
+            <label>Prénom :</label>
+            <input type="text" name="prenom" class="w-full border p-2 rounded" required>
+            <p class="text-red-500 text-sm error-message" id="prenomError"></p>
+        </div>
+
+        <div>
+            <label>Nom :</label>
+            <input type="text" name="nom" class="w-full border p-2 rounded" required>
+            <p class="text-red-500 text-sm error-message" id="nomError"></p>
+        </div>
+
+        <div>
+            <label>Email :</label>
+            <input type="email" name="email" class="w-full border p-2 rounded" required>
+            <p class="text-red-500 text-sm error-message" id="emailError"></p>
+        </div>
+
+        <div>
+            <label>Mot de passe :</label>
+            <input type="password" name="password" class="w-full border p-2 rounded" required>
+            <p class="text-red-500 text-sm error-message" id="passwordError"></p>
+        </div>
+
+        <div>
+            <label>Confirmation :</label>
+            <input type="password" name="confirm" class="w-full border p-2 rounded" required>
+            <p class="text-red-500 text-sm error-message" id="confirmError"></p>
+        </div>
+
+        <button type="submit" class="bg-teal-500 text-white py-2 rounded hover:bg-teal-600">S’inscrire</button>
+        <p class="text-sm text-center mt-2">Déjà un compte ? <a href="connexion.php" class="text-indigo-600 hover:underline">Connectez-vous</a></p>
+    </form>
+</section>
+
+</body>
+</html>
