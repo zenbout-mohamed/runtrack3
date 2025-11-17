f47a51ff5e43b041d88a81a4799dfa7ce70b479b
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-17 16:15:19 +0100

    ajout de la condition dans le fichier main.js

--- jour08/Job05/assets/js/main.js
+++ jour08/Job05/assets/js/main.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const menu = document.getElementById("mobileMenu");
 
+
+    if(hamburger && mobileMenu){
     hamburger.addEventListener("click", () => {
         menu.classList.toggle("hidden");
     });
+    }
 });
