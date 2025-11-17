d6f2ecc1acdbc8e7d748c91aa9a0f58e6ee903a1
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-17 14:52:53 +0100

    fichier script js corrigé

--- jour08/Job05/assets/js/main.js
+++ jour08/Job05/assets/js/main.js
@@ -1,6 +1,8 @@
-const hambuger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("mobileMenu");
 
-hamburger.addEventListener("click",() => {
-    menu.classList.toggle("hidden");
+    hamburger.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
 });
