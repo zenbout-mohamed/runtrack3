5169fd8c55481ca76e965267820aae3e8613aebd
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-20 14:43:26 +0100

    script corrigé

--- jour08/Job05/assets/js/main.js
+++ jour08/Job05/assets/js/main.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const menu = document.getElementById("mobileMenu");
 
-
-    if(hamburger && mobileMenu){
-    hamburger.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-    });
+    if (hamburger && menu) {
+        hamburger.addEventListener("click", () => {
+            menu.classList.toggle("hidden");
+        });
     }
 });
