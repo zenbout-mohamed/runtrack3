e9ecc9bf95d13601bcc45d9f3f13fca7aaee69c2
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-20 11:48:18 +0100

    page config.php corrigé

--- jour08/Job05/config.php
+++ jour08/Job05/config.php
@@ -1,21 +1,21 @@
 <?php 
 
 $db = "laracon";
-$host ="127.0.0.1";
+$host = "127.0.0.1";
 $user = "root";
 $pass = "";
 
-
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-$option = [
+$options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO:: ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $option);
-} catch (PDOException $e){
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
     http_response_code(500);
-    echo "Erreur à la connexion à la base de donnée : " . htmlspecialchars($e->getMessage());
+    echo "Erreur à la connexion : " . htmlspecialchars($e->getMessage());
+    exit;
 }
