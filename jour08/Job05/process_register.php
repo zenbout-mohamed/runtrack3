ed8bc276857adb871491fccf9007eb22fc578c46
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-20 10:34:53 +0100

    réalisation fichier process_register.php

--- jour08/Job05/process_register.php
+++ jour08/Job05/process_register.php
@@ -1,9 +1,41 @@
 <?php
-require_once "config.php";
+require_once "./config.php";
 header('Content-Type: application/json');
 session_start();
 
 $errors = [];
+
+$prenom = trim($_POST['prenom'] ?? '');
+$nom = trim($_POST['nom'] ?? '');
+$civilite = trim($_POST['civilite'] ?? '');
+$adresse = trim($_POST['adresse'] ?? '');
+$email = trim($_POST['email'] ?? '');
+$password = $_POST['password'] ?? '';
+$confirm = $_POST['confirm'] ?? '';
+$passions = $_POST['passions'] ?? [];
+
+//  Message d'erreur 
+
+if(strlen($prenom) < 2)$errors['prenom'] = "Prenom trop court";
+if(strlen($nom) < 2)$errors['nom'] = "nom trop court";
+if(strlen($password) < 6)$errors['password'] = "mot de passe trop court";
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['email'] = "E-mail invalide.";
+if($password !== $confirm)$errors['confirm'] = "Les mots de passes ne correspondent pas.";
+
+// Email unique 
+
+if(!empty($errors)){
+    $stmt = $pdo->prepare("SELECT id FROM users WHERE email = ?");
+    $stmt->execute(['email']);
+    if($stmt->fetch()) $errors['email'] = "Cet E-Mail est déja utilisé.";
+    
+}
+if(!empty($errors)){
+    echo json_encode(['success' => false, 'errors' => $errors]);
+    exit;
+}
+
+
 
 
 
