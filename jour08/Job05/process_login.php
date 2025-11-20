050819614d507bd2703bd7dd3df95d2458a7c95e
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-11-20 11:42:12 +0100

    page process_login.php réalisé

---
+++ jour08/Job05/process_login.php
@@ -0,0 +1,39 @@
+<?php
+require_once "./config.php";
+header('Content-Type: application/json');
+session_start();
+
+$errors = [];
+$email = trim($_POST['email'] ?? '');
+$password = $_POST['password'] ?? '';
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['email'] = "E-mail invalide.";
+if(empty($password)) $errors['password'] = "Mot de passe requis.";
+
+if (!empty($errors)){
+    echo json_encode(['success' => false, 'errors' => $errors]);
+    exit;
+}
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+$stmt->execute([$email]);
+$user = $stmt->fetch();
+
+if(!$user || !password_verify($password, $user['password'])){
+    echo json_encode(['success' => false, 'errors' => ['email' => 'Identitfiant Incorrect. ']]);
+    exit;
+}
+
+// Condition (connexion réussie):
+
+$_SESSION['id'] = $user['id'];
+$_SESSION['prenom'] = $user['prenom'];
+echo json_encode(['success' => true]);
+exit;
+
+
+
+
+
+
+?>
